38a503fa7605f47394caceb7cc3c387841cfbbb2
bainro <bainro@onid.oregonstate.edu>
2018-02-08 00:19:07 -0800

    Climbers die upon boundary collision

--- js/climber-behavior.js
+++ js/climber-behavior.js
@@ -12,12 +12,8 @@ define(
 
             return {
                 init: function (options) {
-                    var self = this;
+
                     parent.init.call(this, options);
-                    // the player will be passed in via the config options
-                    // so we need to store the player
-                    var player = self.player = options.player;
-                    self.gameover = false;
 
                     // events
                     document.addEventListener('keydown', function (e) {});
@@ -27,11 +23,59 @@ define(
                 // this is automatically called by the world
                 // when this behavior is added to the world
                 connect: function (world) {
-
                     // we want to subscribe to world events
-                    world.on('collisions:detected', this.checkPlayerCollision, this);
+                    world.on('collisions:detected', this.checkClimberCollision, this);
                     world.on('integrate:positions', this.behave, this);
                 },
+
+                disconnet: function (world) {
+                    // unsub from world events
+                    world.off('collisions:detected', this.checkClimberCollision, this);
+                    world.off('integrate:positions', this.behave, this);
+                },
+
+                checkClimberCollision: function (data) {
+
+                    //console.log(data);
+
+                    var self = this
+                        , world = self._world
+                        , collisions = data.collisions
+                        , col
+                        ;
+
+                    for (var i = 0, l = collisions.length; i < l; ++i) {
+                        col = collisions[i];
+
+                        // 
+                        if (col.bodyA.gameType !== 'circle' &&
+                            col.bodyB.gameType !== 'circle' &&
+                            (col.bodyA.gameType === 'climber' || col.bodyB.gameType === 'climber')
+                        ) {
+                            console.log('Climber hit border');
+                            /*player.blowUp();
+                            world.removeBehavior(this);
+                            // when we crash, we'll publish an event to the world
+                            // that we can listen for to prompt to restart the game
+                            world.emit('lose-game');*/
+                            return;
+                        }
+                    }
+                },
+
+                behave: function () {
+                    var climbers = this.getTargets(),
+                    world = this._world;
+                    for (var i = 0; i < climbers.length; i++){
+                        climbers[i].grow();
+                        if ( climbers[i].aabb().y - climbers[i].geometry.height/2 < 0 ) {
+                            climbers[i].die();
+                            world.removeBehavior(this);
+                        }
+                        //console.log(climbers[i].aabb().y);
+                    }
+                },
+
             };
 
         });
--- js/climber.js
+++ js/climber.js
@@ -11,6 +11,7 @@ define(
 
             // private helpers
 
+
             //what type of options can we imagine passing our climbers upon init?
 
             //What properties and methods will a climber need?
@@ -19,28 +20,60 @@ define(
             /*
             -They grow in one direction until they collide with either a ball, a viewport boundary, 
             a dead climber, or another live climber.
+            -Need to remove the climber if hit by a ball
+            -Need to detect live climber collision, and prevent growth, or set it back X pixels
+            -Need to detect boundary and dead climber collisions. Could just be the default case.
+            -Create indestructable head region using another static img with slightly smaller
+            dimensions to ensure that no climber collisions are prevented. The climber bodies 
+            should update its position at the same time as its own.
             -Their top half*unit**2 need to remain indestructable and distinguished
-            -They need a direction (N,W,S,E)
-            -They need a die method that removes their event listeners, sets their growth to 0,
+            -They need a direction. This determines whether their position changes constantly, what direction
+            they grow in, and their color.
+            -They need a die method that removes their event listeners and head region, sets their growth to 0,
             and makes them translucent.
             -Need a method to get ball coordinates and pass to flood-fill
             -Need a flood-fill method 
             */
-            
+
             //extension definition
             return {
-               
+
                 init: function (options) {
                     parent.init.call(this, options);
+                    this.direction = options.direction;
+                    this.live = true;
+                    this.growth = .5;
+                    this.gameType = "climber";
                 },
-                // this will turn the ship by changing the
-                // body's angular velocity to + or - some amount
-                turn: function (amount) {
-                    // set the ship's rotational velocity
-                    this.state.angular.vel = 0.2 * amount * deg;
+
+                // Makes climber grow
+                grow: function () {
+                    this.geometry.height += this.growth;
+                    //need view to be recalculated each time growth occurs...
+
+                    var scratch = Physics.scratchpad();
+                    var v = scratch.vector().set(
+                        0,
+                        -this.growth/2
+                    );
+
+                    $(this.view).attr('height', this.geometry.height);
+                    this.state.pos = this.state.pos.vadd(v);
+                    scratch.done();
+
                     return this;
                 },
-                
+
+                die: function () {
+                    
+                    console.log('Bleh! I died :(');
+                    var world = this._world;
+                    world.removeBody(this);
+                    //climber1 = null;
+
+                    return;
+                },
+
             };
 
         });
