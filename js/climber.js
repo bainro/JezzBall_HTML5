790f8db7228a2aaa1f86742184c2dbe799d2716d
bainro <bainro@onid.oregonstate.edu>
2018-02-09 12:36:57 -0800

    Formatted and Commented climber.js

--- js/climber.js
+++ js/climber.js
@@ -9,18 +9,14 @@ define(
         // extend the circle body
         Physics.body('climber', 'rectangle', function (parent) {
 
-            // private helpers
-
-
-            //extension definition
             return {
 
                 init: function (options) {
                     parent.init.call(this, options);
                     this.dir = options.dir;
-                    this.live = true;
                     this.growth = .5;
                     this.gameType = "climber";
+                    view.width = this.geometry.width;
                     view = new Image();
                     switch (this.dir) {
                         case 'N': // North
@@ -34,33 +30,26 @@ define(
                         case 'W': // West
                             break;
                     }
-                    view.width = this.geometry.width;
                     this.view = view;
-                    this.cellLoc = options.cell;
                 },
 
-                // Makes climber grow
                 grow: function () {
 
-                    //need view to be updated each time growth occurs...
                     this.geometry.height += this.growth;
                     $(this.view).attr('height', this.geometry.height);
-                    //this.state.pos = this.state.pos.add(-this.growth/2);
 
                     return this;
                 },
 
                 die: function () {
-                    
-                    var world = this._world,
-                        //cell = cells[this.cellLoc[0]][this.cellLoc[1]],
-                        cell_num = this.geometry.height / unit;
 
-                    //depends on dir
+                    var world = this._world,
+                        num_of_cells = this.geometry.height / unit;
+
                     switch (this.dir) {
                         case 'N':
-                            for (var i = 0; i <= cell_num; i++) {
-                                $(cells[ this.cellLoc[ 0 ] ][ this.cellLoc[ 1 ] - i ]).detach();
+                            for (var i = 0; i <= num_of_cells; i++) {
+                                $(cells[this.source_cell[0]][this.source_cell[1] - i]).detach();
                             }
                             var dead_copy = Physics.body('rectangle', {
                                 x: this.aabb().x
@@ -70,15 +59,14 @@ define(
                                 , restitution: 1
                                 , cof: 0
                                 , treatment: 'static'
-                                //, mass: 100000
                                 , styles: {
                                     fillStyle: 'rgba(0,0,0,0)'
                                 }
                             });
                             break;
                         case 'S':
-                            for (var i = 0; i <= cell_num; i++) {
-                                $(cells[this.cellLoc[0]][this.cellLoc[1] + i]).detach();
+                            for (var i = 0; i <= num_of_cells; i++) {
+                                $(cells[this.source_cell[0]][this.source_cell[1] + i]).detach();
                             }
                             var dead_copy = Physics.body('rectangle', {
                                 x: this.aabb().x
@@ -88,17 +76,16 @@ define(
                                 , restitution: 1
                                 , cof: 0
                                 , treatment: 'static'
-                                //, mass: 100000
                                 , styles: {
                                     fillStyle: 'rgba(0,0,0,0)'
                                 }
                             });
                             break;
                         default:
-                            console.log('You need to code switch in climber.die()');
+                            console.log('You need to code this switch in climber.die()');
                             break;
                     }
-                    
+
                     dead_copy.gameType = 'dead_climber';
                     world.removeBody(this);
                     world.addBody(dead_copy);
