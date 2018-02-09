4727a58404f81b4cabd151abbf751e02b680ab58
bainro <bainro@onid.oregonstate.edu>
2018-02-09 04:49:10 -0800

    Created flood-fill functionality

--- js/climber-behavior.js
+++ js/climber-behavior.js
@@ -73,14 +73,75 @@ define(
 
                 behave: function () {
                     //can pass in climber obj during behav init and pass fx to set this method to dynamically
-                    world = this._world;
+                    var world = this._world,
+                        row,
+                        col;
+
 
                         this.getTargets()[0].grow();
-                        //depends on direction
                         if (this.getTargets()[0].aabb().y - this.getTargets()[0].geometry.height/2 < 0 || 
                             this.getTargets()[0].aabb().y + this.getTargets()[0].geometry.height / 2 > canvasHeight
                         ) {
                             this.getTargets()[0].die();
+
+                        /*
+                        -Get ball positions
+                        -Call flood fill on each ball
+                        -Convert ball coordinates to cells[col][row]
+                        -Check that that cell is contained within #cell_container
+                        -Also check that row and col are within (0,26) & (0,17) respectively
+                        -If not, return
+                        -If true, remove the cell and recursively call flood-fill on:
+                            *cells[col + 1][row]
+                            *cells[col - 1][row]
+                            *cells[col][row + 1]
+                            *cells[col][row - 1]
+                        */
+
+                            //$('#cell_container').children().detach();
+
+                            var balls = world.find(Physics.query({
+                                name: 'circle', // only circles AKA our balls
+                            }));
+
+                            for (var i = 0; i < balls.length; i++) {
+                                //get coord's and convert to matrix indexes
+                                col = Math.round(balls[i].state.pos.x / unit);
+                                row = Math.round(balls[i].state.pos.y / unit);
+                                flood_mark([col, row]);
+                            }
+
+                            for (var i = 0; i < 27; i++) {
+                                for (var j = 0; j < 18; j++) {
+                                    if ($(cells[i][j]).data('marked') != 'true') {
+                                        //console.log('c\'mon');
+                                        $(cells[i][j]).detach();
+                                    }
+                                    else {
+                                        $(cells[i][j]).data('marked', 'false');
+                                    }
+                                }
+                            }
+
+                            function flood_mark(indexes) {
+                                if (indexes[0] >= 0 && indexes[0] <= 26 && indexes[1] >= 0 && indexes[1] <= 17
+                                    && $.contains(document.getElementById('cell_container'), $(cells[indexes[0]][indexes[1]])[0])
+                                    && $(cells[indexes[0]][indexes[1]]).data('marked') != 'true'
+                                ) {
+                                    //set data-visited to true to mark cell as visited
+                                    //remove all cells not marked as visited
+                                    //reset data-visited for all cells upon next climber creation
+                                    $(cells[indexes[0]][indexes[1]]).data('marked', 'true');
+                                    //console.log($(cells[indexes[0]][indexes[1]]).data('marked'));
+                                    //recursive calls
+                                    flood_mark([indexes[0] + 1, indexes[1]]);
+                                    flood_mark([indexes[0] - 1, indexes[1]]);
+                                    flood_mark([indexes[0], indexes[1] - 1]);
+                                    flood_mark([indexes[0], indexes[1] + 1]);
+                                }
+                                return;
+                            }
+
                             world.removeBehavior(this);
                         }
                         
--- js/climber.js
+++ js/climber.js
@@ -12,29 +12,6 @@ define(
             // private helpers
 
 
-            //what type of options can we imagine passing our climbers upon init?
-
-            //What properties and methods will a climber need?
-
-            //What do climbers do?
-            /*
-            -They grow in one direction until they collide with either a ball, a viewport boundary, 
-            a dead climber, or another live climber.
-            -Need to remove the climber if hit by a ball
-            -Need to detect live climber collision, and prevent growth, or set it back X pixels
-            -Need to detect boundary and dead climber collisions. Could just be the default case.
-            -Create indestructable head region using another static img with slightly smaller
-            dimensions to ensure that no climber collisions are prevented. The climber bodies 
-            should update its position at the same time as its own.
-            -Their top half*unit**2 need to remain indestructable and distinguished
-            -They need a direction. This determines whether their position changes constantly, what direction
-            they grow in, and their color.
-            -They need a die method that removes their event listeners and head region, sets their growth to 0,
-            and makes them translucent.
-            -Need a method to get ball coordinates and pass to flood-fill
-            -Need a flood-fill method 
-            */
-
             //extension definition
             return {
 
@@ -124,7 +101,7 @@ define(
                     
                     dead_copy.gameType = 'dead_climber';
                     world.removeBody(this);
-                    world.addBody(dead_copy)
+                    world.addBody(dead_copy);
 
                     return;
                 },
